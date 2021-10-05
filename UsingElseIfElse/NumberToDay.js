24a079032693cc8fd57cbe27f6e3b24879b03bad
namancg <naman.c@perfios.com>
2021-10-05 22:33:20 +0530

    ADDED: code to display which day of the week it is

---
+++ UsingElseIfElse/NumberToDay.js
@@ -0,0 +1,30 @@
+var prompt = require('prompt-sync')();
+let day = prompt("ENTER THE DAY IN NUMBER");
+if(day ==1)
+{
+    console.log("MONDAY")
+}
+else if (day==2)
+{
+    console.log("TUEADAY");
+}
+else if (day==3)
+{
+    console.log("WEDNESDAY");
+}
+else if (day==4)
+{
+    console.log("THURSDAY");
+}
+else if (day==5)
+{
+    console.log("FRIDAY");
+}
+else if (day==6)
+{
+    console.log("SATURDAY");
+}
+else if (day==7)
+{
+    console.log("SUNDAY");
+}
