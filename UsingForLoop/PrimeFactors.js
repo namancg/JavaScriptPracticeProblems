040f938ceafe7c245792eb5f7c9bf99458f70810
namancg <naman.c@perfios.com>
2021-10-05 22:48:48 +0530

    ADDED: code to compute prime factors of a given number

---
+++ UsingForLoop/PrimeFactors.js
@@ -0,0 +1,25 @@
+var prompt = require('prompt-sync')();
+console.log("ENTER THE NUMBER");
+var integer = prompt();
+var primeArray = [];
+let isPrime;
+for (i = 2; i < integer/2; i++) {
+  if (integer % i == 0) {
+
+    for (var j = 2; j <= i / 2; j++) {
+      if (i % j == 0) {
+        isPrime = false;
+      } else {
+        isPrime = true;
+      }
+    }
+    if (isPrime == true) {
+      integer /= i
+      primeArray.push(i);
+    }
+  }
+}
+
+for (var k = 0; k < primeArray.length; k++) {
+  console.log(primeArray[k]);
+}
