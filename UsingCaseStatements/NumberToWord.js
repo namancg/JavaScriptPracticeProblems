d73056798fd33e41f0c40e7bf3a3acac95c03849
namancg <naman.c@perfios.com>
2021-10-05 22:38:42 +0530

    REFACTORED: used switch cases in order to find the word form of the given number

---
+++ UsingCaseStatements/NumberToWord.js
@@ -0,0 +1,55 @@
+var prompt = require('prompt-sync')();
+var number=prompt();
+printWord(number);
+function printWord(number)
+{
+    var i;
+    var length = number.length;
+    for(i =0;i<length;i++)
+    {
+        printValue(number.charAt(i));
+    }
+}
+function printValue(digit)
+{
+    switch(digit)
+    {
+        case '0' : console.log("zero");
+        break;
+
+        case '1':
+            console.log("One ");
+        break;
+ 
+        case '2':
+            console.log("Two ");
+            break;
+
+        case '3':
+            console.log("Three ");
+            break;
+    
+        case '4':
+            console.log("Four ");
+            break;
+
+        case '5':
+            console.log("Five ");
+            break;
+        case '6':
+            console.log("Six ");
+            break;
+    
+        case '7':
+            console.log("Seven ");
+            break;
+    
+        case '8':
+            console.log("Eight ");
+            break;
+    
+        case '9':
+            console.log("Nine ");
+            break;
+    }
+}
