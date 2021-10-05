9f8c3dba6f6274a8df109f7c87137486ca44cf10
namancg <naman.c@perfios.com>
2021-10-05 17:38:54 +0530

    ADDED: code to find min and max of given set of values

--- UsingIfElse/MinimumMaximumValue.js
+++ UsingIfElse/MinimumMaximumValue.js
@@ -24,3 +24,24 @@ else
 {
     console.log("E IS GREATER");
 }
+
+if(a<b && a<c && a<d && a<e)
+{
+    console.log("A IS MINIMUM");
+}
+else if(b<c && b<d && b<e && b<a)
+{
+    console.log("B IS MINIMUM");
+}
+else if (c<d && c<e && c<b && c<a)
+{
+    console.log("C IS MINIMUM");
+}
+else if (d<e && d<a && d<b && d<c)
+{
+    console.log("D IS MINIMUM");
+}
+else
+{
+    console.log("E IS MINIMUM");
+}
