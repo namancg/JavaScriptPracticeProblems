2a8a2f62d89da85f607b0c3a6094ab60e8c58707
namancg <naman.c@perfios.com>
2021-10-05 22:28:48 +0530

    ADDED:code to demonstrate random() function

---
+++ SequencePracticeProbelms/REPLDemo.js
@@ -0,0 +1,9 @@
+let singleDigitNumber = Math.floor(Math.random() * 10);
+console.log("single digit number generated : "+ singleDigitNumber);
+
+let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
+console.log("value after rolling a die: "+ diceNumber);
+
+diceNumber += (Math.floor(Math.random() * 10) % 6) + 1;
+
+console.log("value after rolling twice: "+ diceNumber);
