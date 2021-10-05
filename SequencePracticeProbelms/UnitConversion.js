36c5cdc3db1edd5b24ff9dbc6f05eac99854b7e9
namancg <naman.c@perfios.com>
2021-10-05 22:30:20 +0530

    ADDED: code to convert iinch to feet and square feet and to acres

---
+++ SequencePracticeProbelms/UnitConversion.js
@@ -0,0 +1,8 @@
+let inch = Math.floor(Math.random() * 100);
+let feet = inch / 12;
+console.log("Given inch: "+ inch+" converted to feet: "+ feet);
+let rectangularArea = 60 * 40;
+let totalArea = rectangularArea * 25;
+// one acre equals to 43,560 square feet.
+let areaInAcres = totalArea / 43560;     
+console.log("The area in square feet is: "+ totalArea+" and in acres: "+ areaInAcres);
